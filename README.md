dec0f0e8958eb1fbb59c8a0f99c1318c9c8ee980
vjspranav <pranavasri@live.in>
2023-07-27 22:43:01 +0530

    Added working website

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A simple page, trying to collect all the flash games I've played in my childhood and allow people to also play them offline.
 
-<!-- Stay tuned -->
+# Info
 
-Stay tuned for more info.
+Currently has 253 Games, and the website works
+You can use it at: [link](https://games.stag-os.org)
--- js/player.js
+++ js/player.js
@@ -1,8 +1,24 @@
-window.RufflePlayer = window.RufflePlayer || {};
-window.addEventListener("load", (event) => {
-  const ruffle = window.RufflePlayer.newest();
-  const player = ruffle.createPlayer();
-  const container = document.getElementById("container");
-  container.appendChild(player);
-  player.load("../games/Super Mario 63.swf");
+document.addEventListener("DOMContentLoaded", () => {
+  // Function to get the value of a specific query parameter from the URL
+  const getQueryParam = (name) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+  };
+
+  // Get the file name from the query parameter
+  const file = getQueryParam("file");
+
+  // Load the game using Ruffle
+  window.RufflePlayer = window.RufflePlayer || {};
+  window.addEventListener("load", (event) => {
+    const ruffle = window.RufflePlayer.newest();
+    const player = ruffle.createPlayer();
+    const container = document.getElementById("game-container");
+    container.appendChild(player);
+    player.load("../games/" + file);
+  });
+
+  // Add title to game-title h1
+  const gameTitle = document.getElementById("game-title");
+  gameTitle.innerHTML = file.replace(".swf", "").replace(/_/g, " ");
 });
