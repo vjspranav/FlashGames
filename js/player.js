4afab0b0195375b0b9a5763b3baf46c4e1af8394
vjspranav <pranavasri@live.in>
2023-07-27 19:23:29 +0530

    Add wasm player

---
+++ js/player.js
@@ -0,0 +1,8 @@
+window.RufflePlayer = window.RufflePlayer || {};
+window.addEventListener("load", (event) => {
+  const ruffle = window.RufflePlayer.newest();
+  const player = ruffle.createPlayer();
+  const container = document.getElementById("container");
+  container.appendChild(player);
+  player.load("../games/Super Mario Flash.swf");
+});
